a63c8a525c2ce8d4a350c44d4a670715ebee8fd3
sdlouha <sarka.dlouha@cleverlance.com>
2022-03-13 20:19:30 +0100

    doplneni anotace table

--- src/main/java/com/cleverlance/academy/iotServer/model/WeatherValues.java
+++ src/main/java/com/cleverlance/academy/iotServer/model/WeatherValues.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "weather_values")
 public class WeatherValues {
 
         @Id
